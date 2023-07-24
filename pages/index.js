365ab2982d62f555f10b983224a6039c3ed6d8d5
Eddie Lovejoy <james.lovejoy2@gmail.com>
2023-07-24 08:06:30 -0400

    initial commit of all the files to start the class

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import { data} from "../SpeakerData";
+
+const IndexPage = () => {
+
+    const {id, bio, first ,last, favorite, twitterHandle, company, sessions } = data[0];    
+
+    return (
+        <div>Hello From Pluralsight</div>
+    )
+}
+
+export default IndexPage;
